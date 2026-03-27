5bfc302b44a77bb77fbc7faf97374b06767e9738
pedropiasecki <pedropiaseckiii@gmail.com>
2026-03-27 14:56:47 -0300

    métodos de multiplicação e leitura atualizados
    multiplicacao.py: verificação adicionada e tamanho da matriz C ajustável de acordo com a multiplicação
    leitura.py: adicionado min ou max e legibilidade

--- leitura.py
+++ leitura.py
@@ -5,15 +5,40 @@ def ler_sistema(nome_arquivo):
     with open(nome_arquivo, "r") as f:
         linhas = [l.strip() for l in f.readlines() if l.strip()]
 
-    ## numero de variaveis
+    min_or_max = 0
+
+    """
+    ### Verificar se é min ou max
+    """
+    if re.search(rf"\b{re.escape('min')}\b", linhas[0]):
+        min_or_max = 1 
+    else:
+        min_or_max = 0
+        
+    """
+    ### Encontrar número de váriaveis
+    (\\d+) serve para pegar 1 ou mais digitos apos a variavel x em todas as linhas
+    """
     todas_vars = re.findall(r'x(\d+)', " ".join(linhas))
     n = max(map(int, todas_vars))
 
-    ## cria o vetor de custo
+    """
+    ### cria o vetor de custo
+    [+-]? -> opcionalmente um sinal + ou -.
+    \\s* -> zero ou mais espaços em branco.
+    \\d* -> coeficiente -> zero ou mais dígitos.
+    x -> x que separa os grupos
+    () -> serve para criar grupos
+    """
     c = np.zeros(n)
     termos = re.findall(r'([+-]?\s*\d*)x(\d+)', linhas[0])
 
-    # trata os sinais
+    """
+    ### trata os sinais
+    if -> pega valores positivos do começo até o final, onde coeficiente é 1
+    elif -> pega valores negativos do começo até o final, onde coeficiente é 1
+    else -> | coeficientes |> 1, positivos ou negativos
+    """
     for coef, var in termos:
         coef = coef.replace(" ", "")
         if coef in ["", "+"]:
@@ -25,18 +50,25 @@ def ler_sistema(nome_arquivo):
 
         c[int(var) - 1] += coef
 
-    ## regras do sistema
+    """
+    ### Leitura das regras do sistema
+    separa cada linha em esquerda e direita a partir do sinal. O lado esquerdo é tratado da mesma forma
+    que o vetor de custos, enquanto o lado direito é tratado diretamente
+    A -> lado esquerdo forma uma matriz nxn
+    b -> lado direito forma um vetor n
+    sinais -> vetor que guarda o sinal de cada linha
+    """
     A = []
     b = []
     sinais = []
 
-    # cada linha uma regra
     for linha in linhas[1:]:
-        # identificar sinal da regra
+        """
+        .group(0) retorna o texto que eu match
+        """
         match = re.search(r'(<=|>=|=|<|>)', linha)
         sinal = match.group(0)
 
-        # separa os lados A, b
         esquerda, direita = linha.split(sinal)
 
         linha_A = np.zeros(n)
@@ -57,5 +89,15 @@ def ler_sistema(nome_arquivo):
         b.append(float(direita.strip()))
         sinais.append(sinal)
 
-    return np.array(A), np.array(b), c, sinais
+    return normalizar(np.array(A), sinais, min_or_max)
 
+
+def normalizar(A, sinais, min_or_max):
+    A_normalizada = []
+    
+    print(A)
+    print(sinais)
+    print(min_or_max)
+
+
+ler_sistema('funcoes.txt')
